d78581f6aee9a1309263a4a593aa8383657a0056
Jose Eduardo Martins <m4rt1ns.jose@gmail.com>
2022-03-06 06:53:44 -0300

    update

--- front-end/src/components/publicPages/Login.js
+++ front-end/src/components/publicPages/Login.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 //css
-import './Login.css';
+import styles from './Login.module.css';
 //component
 const Login = () => {
 	return(
-		<div className="contaider">
-        <center><img className="container_logo" src="https://audaces.com/wp-content/themes/Audaces-2018/img/logo.svg" alt="Logo"/></center>
-        <form className="contaider_form">
+		<div className={styles.container}>
+        <center><img className={styles.container_logo} src="https://audaces.com/wp-content/themes/Audaces-2018/img/logo.svg" alt="Logo"/></center>
+        <form className={styles.contaider_form}>
             <fieldset>
                 <legend>Fazer login</legend>
-                <input v-model="email" type="email" name="login" required placeholder="E-mail" /> <br/>
-                <input v-model="password" type="password" name="senha" required placeholder="Senha" minlength="8" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="A senha deve conter 8 ou mais caracteres com pelo menos um número e uma letra maiúscula e minúscula"/> <br/>
-                <div className="forgot_password"> Esqueceu sua senha? </div>
+                <input type="email" required placeholder="Login" /> <br/>
+                <input type="password" required placeholder="Senha" minlength="8" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="A senha deve conter 8 ou mais caracteres com pelo menos um número e uma letra maiúscula e minúscula"/> <br/>
+                <div className={styles.forgot_password}> Esqueceu sua senha? </div>
             </fieldset>
-            <Link to="/register" className="register">Cadastro</Link>
-            <button className="enter" type="submit"> Entrar </button>
+            <Link className={styles.register}to="/register"> Cadastro </Link>
+            <button className={styles.enter} type="submit"> Entrar </button>
         </form>
     </div>
 	)
--- front-end/src/components/publicPages/Register.js
+++ front-end/src/components/publicPages/Register.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 //css
-import './Login.css';
-
+import styles from './Login.module.css';
+//components
 const Register = () => {
 	return(
-		<div className="contaider">
-			<center><img className="container_logo" src="https://audaces.com/wp-content/themes/Audaces-2018/img/logo.svg" alt="Logo"/></center>
+		<div className={styles.container}>
+			<center><img className={styles.container_logo} src="https://audaces.com/wp-content/themes/Audaces-2018/img/logo.svg" alt="Logo"/></center>
 			<form>
 				<fieldset>
 					<legend> Cadastro de Cliente </legend>
-					<input type="text" />
+					<input type="text" required placeholder="Nome da empresa"/><br/>
+					<input type="text" required placeholder="Cnpj"/><br/>
+					<input type="text" required placeholder="Login"/><br/>
+					<input type="password" required placeholder="Password"/><br/>
 				</fieldset>
-				<Link to="/" className="login_back"> Voltar para pagina de login </Link>
+				<Link className="login_back" to="/"> Voltar para pagina de login </Link>
 				<button type="submit"> Cadastrar </button>
 			</form>
 		</div>
